6f070dedbda99d3df4005c78626166a0a373d284
lana-alghobari <lana.alghobari05@gmail.com>
2026-05-25 17:24:44 +0300

    loaded the pool table and added orbit control

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0x222222)
@@ -9,23 +11,61 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 )
-camera.position.z = 5
+camera.position.set(0, 5,10 )
+const loader = new GLTFLoader()
+loader.load(
+  '/models/scene.gltf',
+  (gltf) => {
+    const model = gltf.scene;
+    model.scale.set(1.5, 1.5, 1.5);
+    model.position.set(0, 0, 0);
+    const axesHelper = new THREE.AxesHelper(5);
+    scene.add(axesHelper);
 
-const renderer = new THREE.WebGLRenderer()
+    const gridHelper = new THREE.GridHelper(20, 20);
+    scene.add(gridHelper);
+    model.traverse((child) => {
+      if (child.isMesh) {
+        child.castShadow  = true;
+        child.receiveShadow = true;
+      }
+    });
+    
+    scene.add(model);
+    console.log('success niggas',model);
+  },
+  (xhr) => {
+     console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+  },
+  (error) => {
+    console.log(error);
+  }
+
+);
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+const gridHelper = new THREE.GridHelper(20, 20);
+scene.add(gridHelper);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+scene.add(ambientLight);
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.8);
+dirLight.position.set(5, 10, 7);
+scene.add(dirLight);
+
+const renderer = new THREE.WebGLRenderer({antialias : true})
 renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+document.getElementById('poolTable').appendChild(renderer.domElement)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+
 
 function animate() {
   requestAnimationFrame(animate)
-
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
-
+  controls.update()
   renderer.render(scene, camera)
 }
 
