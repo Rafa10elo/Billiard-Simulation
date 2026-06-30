7b6b77e9012c99253ef767b9e59653078c6c425f
lana-alghobari <lana.alghobari05@gmail.com>
2026-06-30 04:17:14 +0300

    ghdoo basar mrah 2

--- src/Data/TableData.js
+++ src/Data/TableData.js
@@ -13,7 +13,7 @@ export const TableData = {
 };
 
 export const CueData = {
-	modelPath: '/models/cueStick/cueStick.glb',
+	modelPath: '/models/cueStick/stick.glb',
 	texturePath: '/models/cueStick/textures/pool_cue_cue_mat_BaseColor.1001.png',
 	scale: { x: 0.8, y: 0.8, z: 0.8 },
 	position: {  x: 0.72, y: 0.9, z: 0.52  }
